adefe457975f33f2e6e92d13b4b0d322e925e280
scerites <westlove00@yahoo.com.tw>
2016-07-20 16:07:22 +0800

    over

---
+++ Assets/oversize.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oversize: MonoBehaviour {
+	public GameObject prefab001;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Instantiate (prefab001);
+		}
+	
+	}
+}
