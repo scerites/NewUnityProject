5d7121b7aca647763d20906d0329a543f40355fb
scerites <westlove00@yahoo.com.tw>
2016-07-27 16:24:01 +0800

    jump

---
+++ Assets/jump.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float hor = Input.GetAxis ("Horizontal");
+		if ( Mathf.Abs(hor)> 0) 
+		{
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + hor);
+
+		}
+	}
+}
